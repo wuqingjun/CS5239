f9e5d1b1115b98aa26dc7ec970466a2655dce3b3
wade wu <wadewu@wades-MacBook-Pro.local>
2016-11-04 01:58:09 -0700

    first draft of hw06

---
+++ HW06/Xcode/GLTransformations/Resources/lamp/lamp.h
@@ -0,0 +1,14 @@
+//
+//  lamp.h
+//  HW06
+//
+//  Created by wade wu on 11/3/16.
+//  Copyright © 2016 Ricardo Rendon Cepeda. All rights reserved.
+//
+
+
+const int lampVertexCount;
+const float lampPositions[36 * 360];
+const float lampTextureCoordinates[24 * 360];
+const float lampNormals[36 * 360];
+
